44e8d8b7fac36331aa35a1b4dec8028662d74916
Mayank-chn <mynkchnmighty@gmail.com>
2024-08-30 23:17:40 +0530

    adding installation file

---
+++ 11-installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+then
+echo "run the script with root previlage"
+exit 1
+fi
+
+dnf list installed git
+if[ $? -ne 0 ]
+then
+echo "git is not installed please install"
+dnf install git -y
+if[ $? -ne 0 ]
+then
+echo "git installation is not success, check once"
+exit 1
+
+else
+echo "instalation success"
