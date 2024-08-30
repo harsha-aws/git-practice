1f5d26c8ffad7a8281bed9fe39fccd00fd651138
Mayank-chn <mynkchnmighty@gmail.com>
2024-08-30 23:30:55 +0530

    adding installation file

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 USERID=$(id -u)
+#echo "User ID is: $USERID"
 
 if [ $USERID -ne 0 ]
 then
-echo "run the script with root previlage"
-exit 1
+    echo "Please run this script with root priveleges"
+    exit 1
 fi
 
-dnf list installed git
+dnf list installed git # Just checking whether installed or not
 
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]
 then
-echo "git is not installed please install"
-dnf install git -y
-if[ $? -ne 0 ]
-then
-echo "git installation is not success, check once"
-exit 1
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
 else
-echo "instalation success"
+    echo "Git is already installed, nothing to do.."
 fi
-else
-echo "git already installed nothing to do"
-fi
-
