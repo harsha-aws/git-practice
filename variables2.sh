79035d0277f047fc3538d6ac1946df0d5a534875
Mayank-chn <mynkchnmighty@gmail.com>
2024-08-30 17:47:29 +0530

    added variables files

--- variables2.sh
+++ variables2.sh
@@ -5,6 +5,6 @@ PERSON1=ramesh
 PERSON2= naresh
 
 echo "$PERSON1 :: Hi ${PERSON2} how are you"
-echo "${PERSON2} :: Hi {$PERSON2} I am doing good, How are you doing"
+echo "${PERSON2} :: Hi $PERSON2 I am doing good, How are you doing"
 echo "$PERSON1 :: i am fine ${PERSON2} whats going on"
-echo "$PERSON2 :: I started learning shell script {$PERSON1}"
+echo "${PERSON2} :: I started learning shell script {$PERSON1}"
