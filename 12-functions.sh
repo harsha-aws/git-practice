df02c59d2c1cf23653b8cfde03387faf3687377d
Mayank-chn <mynkchnmighty@gmail.com>
2024-08-31 17:52:15 +0530

    added functions file

---
+++ 12-functions.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+USERID=$(id -u)
+
+CHECK_ROOT(){
+    if [ USERID -ne 0 ]
+    then
+          echo " run this crript with root access"
+          exit 1
+    fi
+}
+VALIDATE(){
+    IF [ $1 -ne 0 ]
+    then
+        echo "$2 is failed"
+        exit 1
+    else
+        echo " S2 is success"
+    fi
+}
+CHECK_ROOT
+dng list installed git
+
+if [ $? -ne 0 ]
+then
+    echo " git is not installed please install"
+    dnf install git -y
+    VALIDATE $? "installing git"
+else
+    echo "git is installed already"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo " nginx is not installed"
+    dnf install nginx -y
+    VALIDATE $? "installing nginx"
+else
+    echo "nginx is already installed"
+fi 
